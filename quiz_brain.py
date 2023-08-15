0b7a0cd75f8e84aeb5b9618eada08462e629fc7f
Luewngier <g217881111@gmail.com>
2023-08-16 01:44:19 +0800

    Complete basic application

--- quiz_brain.py
+++ quiz_brain.py
@@ -10,7 +10,8 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
@@ -20,4 +21,5 @@ class QuizBrain:
             print("That's wrong.")
         print(f"the correct answer was: {correct_answer}.")
         print(f"Your current score is: {self.score}/{self.question_number}.")
+        print("\n")
 
